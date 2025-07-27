c6f41486054a5b5b331743390505772c05c1a25b
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-07-28 02:31:30 +0500

    react

--- React/react-project-one/src/App.jsx
+++ React/react-project-one/src/App.jsx
@@ -1,12 +1,15 @@
 import './App.css'
 import User_card from './Components/User_card'
+import m4 from './assets/m4.jpeg'
+import m4f from './assets/m4fool.jpg'
+import akm from './assets/akm.webp'
 
 function App() {
   return (
     <div className='container'>
-      <User_card name="HONEYPOT"/>
-      <User_card name="honeypot"/>
-      <User_card name="honeyPOT"/>
+      <User_card name="HONEYPOT" desc="1st" image={m4} style={{"border-radius":"10px"}}/>
+      <User_card name="honeypot" desc="2st" image={m4f} style={{"border-radius":"10px"}}/>
+      <User_card name="honeyPOT" desc="3st" image={akm} style={{"border-radius":"10px"}}/>
     </div>
    
   )
--- React/react-project-one/src/Components/User_card.jsx
+++ React/react-project-one/src/Components/User_card.jsx
@@ -1,14 +1,12 @@
 import React from 'react'
 import './User_card.css'
-import pharoh from '../assets/m4.jpeg'
-
 
 const user_card = (props) => {
   return (
-    <div className='class'>
+    <div className='class' style={props.style}>
       <p id='user-name'>{props.name}</p>
-      <img id='user-image' src={pharoh} alt="" />
-      <p id='user-description'>THIS IS HONEYPOT DESCRIPTION</p>
+      <img id='user-image' src={props.image} alt="" />
+      <p id='user-description'>{props.desc}</p>
       
     </div>
   )
