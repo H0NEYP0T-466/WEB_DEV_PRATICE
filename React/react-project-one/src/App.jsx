ea2ae32b5f2d9335b20c0221d3147907c5295444
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-07-29 01:45:53 +0500

    State_lifting

--- React/react-project-one/src/App.jsx
+++ React/react-project-one/src/App.jsx
@@ -4,20 +4,16 @@ import m4f from './assets/m4fool.jpg'
 import akm from './assets/akm.webp'
 import Counter from './Components/Counter'
 import React, { useState } from 'react';
+import State_lifting from './Components/State_lifting'
 
 function App() {
-  const [num,set] = useState(0)
-
-  function newfun()
-  { 
-    set(num+1);
-    
-  }
+  const [val , fun] = useState("");
+ 
   return (
     <div>
-      <Counter x={newfun} text="click me">
-        <h1>{num}</h1>
-        </Counter>  
+       <State_lifting text="Child#1" val={val} fun={fun}/>
+       <State_lifting text="Child#2" val={val} fun={fun}/>
+       <p>Parent component: {val}</p>
    </div>
   )
 }
@@ -28,3 +24,17 @@ export default App
       <User_card name="honeypot" desc="2st" image={m4f} style={{"border-radius":"10px"}}/>
       <User_card name="honeyPOT" desc="3st" image={akm} style={{"border-radius":"10px"}}/>
       */
+
+
+/*const [num,set] = useState(0)
+
+  function newfun()
+  { 
+    set(num+1);
+    
+  }
+    
+  
+  <Counter x={newfun} text="click me">
+        <h1>{num}</h1>
+        </Counter> */
