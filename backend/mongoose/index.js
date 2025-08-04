4b6006f71ef5f15febf38513b9f95ebc3dcb53fa
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-05 03:33:24 +0500

    off

--- backend/mongoose/index.js
+++ backend/mongoose/index.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.dburl)
+mongoose.connect('mongodb://127.0.0.1:27017/test')
   .then(() => {
     console.log('MongoDB connected');
     app.listen(8000, () => console.log('Server running on port 8000'));
@@ -15,7 +15,7 @@ mongoose.connect(process.env.dburl)
     console.error('MongoDB connection error:', err);
     process.exit(1);
   });
-  
+
 
 app.post('/userData', async (req, res) => {
   try {
@@ -34,4 +34,30 @@ app.post('/userData', async (req, res) => {
     console.error('Error saving user data:', error);
     res.status(500).send(`Error saving user data: ${error.message}`);
   }
+});
+
+
+app.delete('/deleteUser', async (req, res) => {
+  try {
+    const { email } = req.body;
+    const result = await UserData.deleteOne({ email: email });
+    res.status(200).send(`User with email ${email} deleted successfully`);
+  } catch (error) {
+    console.error('Error deleting user data:', error);
+    res.status(500).send(`Error deleting user data: ${error.message}`);
+  }});
+
+app.get('/findUser', async (req, res) => {
+  try {
+    const userData = await UserData.find();
+    res.status(200).json(userData);
+  } catch (error) {
+    console.error('Error fetching user data:', error);
+    res.status(500).send(`Error fetching user data: ${error.message}`);
+  }
+});
+
+app.put('/updateUser', async (req, res) => {
+    let result = await UserData.updateOne({name:"honeypot"}, {$set: {age: 39}});
+    res.send('User updated: ' + result.modifiedCount);
 });
