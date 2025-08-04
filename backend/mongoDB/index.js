9ec98474f077a58cf5117149a1236041e3ac8547
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-05 01:11:46 +0500

    db

--- backend/mongoDB/index.js
+++ backend/mongoDB/index.js
@@ -5,6 +5,7 @@ let app=express();
 app.use(express.json());
 
 
+
 app.get('/studentRead', async(req, res) => {
 
      let db = await dbConnection();
@@ -13,7 +14,7 @@ app.get('/studentRead', async(req, res) => {
     res.send(data);
 });
 
-app.post('/studentInsert', async(req, res) => {
+app.post('/studentInsert',middleware, async(req, res) => {
     let db = await dbConnection();
     let collection = db.collection('students');
     let object=
@@ -27,6 +28,21 @@ app.post('/studentInsert', async(req, res) => {
     console.log(result);
     res.send('Student Insert API'+result);
 });
+
+async function middleware(req, res, next) {
+    let db = await dbConnection();
+    let collection = db.collection('students');
+    let a=await collection.find({sName:req.body.sName}).toArray();
+    if(a.length === 0)
+    {
+        next();
+    }
+    else
+    {
+        return res.status(401).send('Unauthorized: Student already exists');
+    }
+    
+}
 
 
 app.listen(8000)
