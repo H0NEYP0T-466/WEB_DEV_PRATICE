c5711cc7c8f0a6080378a406bbe1ce8d1a055493
H0NEP0T-466 <fa-23-bscs-466@lgu.edu.pk>
2025-08-05 01:04:57 +0500

    db

--- backend/mongoDB/index.js
+++ backend/mongoDB/index.js
@@ -1,15 +1,31 @@
 let express=require('express');
+let dbConnection = require('../mongoDB/dbConnection');
 let app=express();
 
 app.use(express.json());
 
 
-app.get('/studentRead', (req, res) => {
-    res.send('Student Read API');
+app.get('/studentRead', async(req, res) => {
+
+     let db = await dbConnection();
+    let collection = db.collection('students');
+    let data= await collection.find().toArray();
+    res.send(data);
 });
 
-app.post('/studentInsert', (req, res) => {
-    res.send('Student Insert API');
+app.post('/studentInsert', async(req, res) => {
+    let db = await dbConnection();
+    let collection = db.collection('students');
+    let object=
+    {
+        sName:req.body.sName,
+        sAge:req.body.sAge,
+    }
+
+    console.log(object);
+    let result = await collection.insertOne(object);
+    console.log(result);
+    res.send('Student Insert API'+result);
 });
 
 
